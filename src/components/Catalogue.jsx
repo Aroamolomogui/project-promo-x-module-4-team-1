c0af1b6f60912ad5dfccf726fbfc6b603658698e
OtiliaNicola <julio.guerrero@ezentis.com>
2024-05-30 13:55:55 +0200

    Style

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -2,7 +2,15 @@
 
 function Catalogue() {
   
-    return <div>Catalogue</div>;
+    return <section className="catalogue">
+      <h3 className="title__catalogue">Catálogo de Proyectos</h3>
+     <article className="card__container">
+      <div className="card__project">Proyecto 1</div>
+      <div className="card__project">Proyecto 2</div>
+      <div className="card__project">Proyecto 3</div>
+      <div className="card__project">Proyecto 4</div>
+      </article> 
+    </section>;
   }
   
   export default Catalogue;
