d2d0a561e384ea0e26dcf2e94981dd8701e7704b
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-31 13:41:39 +0200

    add change to css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,12 +6,16 @@ function Landing () {
     <>
     <main className="main--landing">
       <h1 className="title">Mis proyectos molones</h1>
-      <Link to="/main" className="button--link">
-        <button className="button--link">Nuevo proyecto</button>
+      <p className="title--landing">Escaparate en línea para recoger ideas a través de la tecnología.</p>
+
+      <div className="button--landing">
+      <Link to="/main" >
+        <button className="button--link__landing">Nuevo proyecto</button>
       </Link>
-      <Link to="/catalogue" className="button--link">
-       <button className="button--link">Catálogo de Proyectos</button>
+      <Link to="/catalogue" >
+       <button className="button--link__landing">Catálogo de Proyectos</button>
       </Link>
+      </div>
       </main>
     </>
   )
