e80bc631154cd0ae6ba1875bbdcfa8ea70995345
Delia Garcia <deliagarciaburgos@gmail.com>
2024-06-10 22:41:24 +0200

    update button ver proyectos link to catalogue

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -4,18 +4,18 @@ import "../styles/App.scss";
 function Landing () {
   return (
     <>
-    <main className="main--landing">
-      <h1 className="title">Mis proyectos molones</h1>
-      <p className="title--landing">Escaparate en línea para recoger ideas a través de la tecnología.</p>
+      <main className="main--landing">
+        <h1 className="title">Mis proyectos molones</h1>
+        <p className="title--landing">Escaparate en línea para recoger ideas a través de la tecnología.</p>
 
-      <div className="button--landing">
-      <Link to="/main" >
-        <button className="button--link__landing">Nuevo proyecto</button>
-      </Link>
-      <Link to="/catalogue" >
-       <button className="button--link__landing">Catálogo de Proyectos</button>
-      </Link>
-      </div>
+        <div className="button--landing">
+          <Link to="/main" >
+            <button className="button--link__landing">Nuevo proyecto</button>
+          </Link>
+          <Link to="/catalogue" >
+            <button className="button--link__landing">Catálogo de Proyectos</button>
+          </Link>
+        </div>
       </main>
     </>
   )
